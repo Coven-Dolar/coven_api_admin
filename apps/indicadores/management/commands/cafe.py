35f2e045bbb177b9a2bfab4f8f6f473f247b3a09
angel <angel@alphacode.com.br-at-503688510464>
2020-02-16 15:27:52 -0300

    pruebas de cafe

--- apps/indicadores/management/commands/cafe.py
+++ apps/indicadores/management/commands/cafe.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     # A commands must define handle()
     def handle(self, *args, **options):
         # Dow Jones Commodity Index Coffee
-        r = requests.get('https://markets.ft.com/data/indices/tearsheet/summary?s=DJCIKC:DJI')
+        '''r = requests.get('https://markets.ft.com/data/indices/tearsheet/summary?s=DJCIKC:DJI')
         soup = BeautifulSoup(r.content, 'html.parser')
         span = soup.find(class_='mod-ui-data-list__value')
         ValoresMercado(
@@ -20,4 +20,19 @@ class Command(BaseCommand):
             precio=span.get_text(),
             par='USD/QQ',
             mercado=Commodities.objects.get(abreviatura='C')
-        ).save()
+        ).save()'''
+        headers = {'Content-Type': 'application/json',
+                   'Connection': 'keep-alive',
+                   'Server': 'nginx',
+                   'Via': '1.1 a205b777009b4117039d629e4ab51416.cloudfront.net (CloudFront)',
+                   'Vary': 'Accept-Encoding',
+                   'X-Frame-Options': 'SAMEORIGIN ALLOW-FROM http://info.barchart.com',
+                   'Set-Cookie': 'laravel_session=eyJpdiI6IlR2emdqbkkyV1NFcTdVdDh5ZGoxOHc9PSIsInZhbHVlIjoiRXlDYXg3Y1Q3dHFCempEVzVzdTVZZUozbGgzK1BlUXdBMXBaVCtpWDd3d2lYSTcwUlJtWEdmcGVHOGVVTHN0NSIsIm1hYyI6IjY0NDk3MjViNWUwNTBiN2NhNDA1OTVjZjc3NDc4NjU0OTYzMzEwNWZhYmJjYjYxMDI2ZjNhNzhmNTlhOGQ5MTIifQ%3D%3D; expires=Sun, 16-Feb-2020 20:18:52 GMT; Max-Age=7200; path=/; secure; httponly'}
+
+        r = requests.get('https://www.barchart.com/symbols/KCY00/modules?symbolType=2&symbolCode=FUT&hasOptions=1', headers=headers)
+        print(r)
+        #soup = BeautifulSoup(r.content, 'html.parser')
+        #print(soup)
+        #span = soup.find(class_='lastNum pid-8832-last')
+        #print(span)
+
