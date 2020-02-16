98f44a332f6c4bfffb1f08f9a24880c5f902751f
angel <angel@alphacode.com.br-at-503688510464>
2020-02-16 02:09:14 -0300

    cafe

---
+++ apps/indicadores/management/commands/cafe.py
@@ -0,0 +1,23 @@
+#!/usr/bin python3
+import os
+
+from django.core.management.base import BaseCommand
+from bs4 import BeautifulSoup
+from apps.indicadores.models import ValoresMercado, Commodities
+import requests
+
+class Command(BaseCommand):
+    help = "Obtiene el valor del cacao"
+
+    # A commands must define handle()
+    def handle(self, *args, **options):
+        # Dow Jones Commodity Index Coffee
+        r = requests.get('https://markets.ft.com/data/indices/tearsheet/summary?s=DJCIKC:DJI')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        span = soup.find(class_='mod-ui-data-list__value')
+        ValoresMercado(
+            tipo_mercado='I',
+            precio=span.get_text(),
+            par='USD/QQ',
+            mercado=Commodities.objects.get(abreviatura='C')
+        ).save()
