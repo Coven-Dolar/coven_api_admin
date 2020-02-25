4ace02e42f8d9a41101564d3886696880576124f
angel <angel@alphacode.com.br-at-503688510464>
2020-02-25 10:49:59 -0300

    deploy media_root

--- coven/urls.py
+++ coven/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('api/v1/blog/', include('apps.blog.urls')),
     path('api/v1/indicadores/', include('apps.indicadores.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
