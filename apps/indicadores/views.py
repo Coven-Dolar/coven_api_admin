f4bc07f2d3c644833329d3bdd4b149aef25c801e
angel <angel@alphacode.com.br-at-503688510464>
2020-02-25 09:05:45 -0300

    end point de mercado internacional y mercado nacional

--- apps/indicadores/views.py
+++ apps/indicadores/views.py
@@ -1,32 +1,33 @@
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models import Max
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import ListView
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from django.db import connection
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from apps.indicadores.models import ValoresMercado
-from apps.indicadores.serializers import MercadosSerializers
 import json
-from django.core import serializers
-
-
-'''class Internacional(ListAPIView):
-    permission_classes = (AllowAny,)
-    serializer_class = MercadosSerializers
-    queryset = ValoresMercado.objects.filter(tipo_mercado='I').values_list('precio', 'par', 'movilidad').order_by('-fecha')'''
-
 
 class Internacional(APIView):
-    permission_classes = (AllowAny,)
 
     def get(self, request):
-        data = ValoresMercado.objects.filter(tipo_mercado='I', fecha=ValoresMercado.objects.aggregate(max_fecha=Max('fecha')) )\
-            .values('precio', 'par', 'mercado__nombre').order_by('-fecha')
-        print(data.query)
-        #return Response(json.loads(json.dumps(list(data), cls=DjangoJSONEncoder)))
+        cursor = connection.cursor()
+        cursor.execute("SELECT 	nombre, par, precio, movilidad "
+                                          "FROM in_commodities INNER JOIN in_valores_mercado ON mercado_id = in_commodities.abreviatura "
+                                          "WHERE tipo_mercado='I' and fecha in (SELECT max(fecha) from in_valores_mercado where mercado_id = in_commodities.abreviatura)")
+        columns = [col[0] for col in cursor.description]
+
+        data = [dict(zip(columns, row))
+                for row in cursor.fetchall()
+                ]
+        return Response(json.loads(json.dumps(list(data), cls=DjangoJSONEncoder)))
+
+class Nacional(APIView):
+
+    def get(self, request):
+        cursor = connection.cursor()
+        cursor.execute("SELECT 	nombre, par, precio, movilidad "
+                                          "FROM in_commodities INNER JOIN in_valores_mercado ON mercado_id = in_commodities.abreviatura "
+                                          "WHERE tipo_mercado='N' and fecha in (SELECT max(fecha) from in_valores_mercado where mercado_id = in_commodities.abreviatura)")
+        columns = [col[0] for col in cursor.description]
+
+        data = [dict(zip(columns, row))
+                for row in cursor.fetchall()
+                ]
         return Response(json.loads(json.dumps(list(data), cls=DjangoJSONEncoder)))
