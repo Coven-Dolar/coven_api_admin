69ad66458c8d9f4a3e201ab24566802e33fee033
angel <angel@alphacode.com.br-at-503688510464>
2020-02-25 10:00:15 -0300

    url para el mercado nacional

--- apps/indicadores/urls.py
+++ apps/indicadores/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.indicadores.views import Internacional
+from apps.indicadores.views import Internacional, Nacional
 
 urlpatterns = [
-    path('mercados/internacionales/', Internacional.as_view())
+    path('mercados/internacionales/', Internacional.as_view()),
+    path('mercados/nacionales/', Nacional.as_view())
 ]
