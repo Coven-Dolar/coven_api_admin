ebaf277ab30146e1f5a028102eed5ba4d9d2773f
angel <angel@alphacode.com.br-at-503688510464>
2020-04-25 22:43:58 -0300

    icon notificacion

--- apps/indicadores/management/commands/monitor.py
+++ apps/indicadores/management/commands/monitor.py
@@ -59,6 +59,7 @@ class Command(BaseCommand):
         from fcm_django.models import FCMDevice
         device = FCMDevice.objects.all().first()
         device.send_message(title="Actualización de Precio.",
+                            icon="https://coven.jaspesoft.com/static/imagen/icon.png",
                             body="Dolar: "+str(usd)+" \n"
                                                "Paypal "+str(paypal)+" \n"
                                                 "Euro " + str(eur))
