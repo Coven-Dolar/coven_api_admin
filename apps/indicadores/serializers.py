b75af3397dc42758d3567cd0b69a1e0f3d4d3698
angel <angel@alphacode.com.br-at-503688510464>
2020-06-07 09:54:42 -0300

    endpoint para retornar las

--- apps/indicadores/serializers.py
+++ apps/indicadores/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, Serializer
 
-from apps.indicadores.models import ValoresMercado
+from apps.indicadores.models import ValoresMercado, Leyendas
+
 
 class MercadosSerializers(Serializer):
     precio = serializers.DecimalField(max_digits=19, decimal_places=2)
     par = serializers.CharField(max_length=20)
     movilidad = serializers.DecimalField(max_digits=19, decimal_places=2)
+
+
+class LeyendaMercadoSerializer(ModelSerializer):
+
+    class Meta:
+        model = Leyendas
+        fields = '__all__'
--- apps/indicadores/urls.py
+++ apps/indicadores/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.indicadores.views import Internacional, Nacional
+from apps.indicadores.views import Internacional, Nacional, LeyendaMercado
 
 urlpatterns = [
     path('mercados/internacionales/', Internacional.as_view()),
-    path('mercados/nacionales/', Nacional.as_view())
+    path('mercados/nacionales/', Nacional.as_view()),
+    path('mercados/leyenda/<leyenda>/', LeyendaMercado.as_view()),
 ]
