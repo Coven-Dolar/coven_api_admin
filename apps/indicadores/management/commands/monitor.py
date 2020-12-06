14fda243dbaefb8da953a6a9bdee23215786da73
abejarano <programador.angel@gmail.com>
2020-12-05 21:37:52 -0300

    pequeño cambio del dolar.

--- apps/indicadores/management/commands/monitor.py
+++ apps/indicadores/management/commands/monitor.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from apps.indicadores.models import ValoresMercado, Commodities
 import requests
 
+
 class Command(BaseCommand):
     help = "Obtiene el valor del cacao"
 
@@ -16,7 +17,7 @@ class Command(BaseCommand):
         span = soup.find(class_='box-calcmd text-center')
         valor = span.get_text()
         valor = valor.split('\n')
-        print(valor)
+
         usd = valor[2]
         eur = valor[3]
         bcv = valor[4]
@@ -24,7 +25,7 @@ class Command(BaseCommand):
 
         eur = float(eur.replace('Monitor Euro:', '').replace(' Bs.S', '').replace('.', '').replace(',', '.'))
         bcv = float(bcv.replace('BCV: ', '').replace(' Bs.S', '').replace('.', '').replace(',', '.'))
-        usd = float(usd.replace('Monitor Dolar: ', '').replace(' Bs.S', '').replace('.', '').replace(',', '.'))
+        usd = float(usd.replace('Monitor Dólar: ', '').replace(' Bs.S', '').replace('.', '').replace(',', '.'))
         paypal = float(paypal.replace('Paypal: ', '').replace(' Bs.S', '').replace('.', '').replace(',', '.'))
 
         if usd > 0:
@@ -58,12 +59,3 @@ class Command(BaseCommand):
                 par='BS/USD',
                 mercado=Commodities.objects.get(abreviatura='BCV')
             ).save()
-
-        '''from fcm_django.models import FCMDevice
-        device = FCMDevice.objects.all().first()
-        device.send_message(title="Actualización de Precio.",
-                            icon="https://coven.jaspesoft.com/static/imagen/icon.png",
-                            body="Dolar: "+str(usd)+" \n"
-                                               "Paypal "+str(paypal)+" \n"
-                                                "Euro " + str(eur))'''
-
