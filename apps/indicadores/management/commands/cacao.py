aa6c652d8957dc9d108768c82ff0ac21ae9ae2c1
abejarano <programador.angel@gmail.com>
2020-12-30 13:37:46 -0300

    fix en el precio del cacao

--- apps/indicadores/management/commands/cacao.py
+++ apps/indicadores/management/commands/cacao.py
@@ -13,8 +13,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         r = requests.get('https://finance.yahoo.com/quote/CC%3DF?p=CC%3DF')
         soup = BeautifulSoup(r.content, 'html.parser')
-        span = soup.find(class_='Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)')
-        cacao = round(float(span.get_text().replace(',', '')) * 0.00453592,2)
+        span = soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)')
+        cacao = round(float(span.get_text().replace(',', '')) * 0.00453592, 2)
 
         ValoresMercado(
             tipo_mercado='I',
