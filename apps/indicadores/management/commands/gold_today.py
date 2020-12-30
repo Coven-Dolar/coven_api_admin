9633b7f5d3bd04ab9a513833acb7ca22dd2db86a
abejarano <programador.angel@gmail.com>
2020-12-30 14:00:26 -0300

    forrmat code.

--- apps/indicadores/management/commands/gold_today.py
+++ apps/indicadores/management/commands/gold_today.py
@@ -6,19 +6,19 @@ from django.core.management.base import BaseCommand
 from apps.indicadores.models import ValoresMercado, Commodities
 import requests
 
+
 class Command(BaseCommand):
     help = "Obtiene el valor del oro en la bolsa de filadelfia"
 
     # A commands must define handle()
     def handle(self, *args, **options):
-       r = requests.get('https://s3.amazonaws.com/dolartoday/data.json')
-       resp = r.json()
-       ORO = resp['GOLD']['rate']
-       ORO = round(ORO / 28.3495,2)
-       ValoresMercado.objects.create(
+        r = requests.get('https://s3.amazonaws.com/dolartoday/data.json')
+        resp = r.json()
+        ORO = resp['GOLD']['rate']
+        ORO = round(ORO / 28.3495, 2)
+        ValoresMercado.objects.create(
             tipo_mercado='N',
             precio=ORO,
             par='USD/G',
             mercado=Commodities.objects.get(abreviatura='ORO')
-       ).save()
-
+        ).save()
