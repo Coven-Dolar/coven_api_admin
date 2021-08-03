14bbc3c4f95217bc0f2666c537d7dbc7358a3b84
jaspesoft <jaspesoft@gmail.com>
2021-08-03 09:09:54 -0300

    logging

--- coven/settings.py
+++ coven/settings.py
@@ -225,20 +225,33 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/applications/coven.log',
+    'version': 1, 'disable_existing_loggers': False,
+    'handlers': {  # Include the default django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+            # But the emails are plain text by default - HTML is nicer
+            'include_html': True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler', 'filename': '/applications/coven.log'
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
+    'loggers': {  # Again, default Django configuration to email unhandled exceptions
+        'django.request': {
+            'handlers': ['mail_admins'], 'level': 'ERROR',
             'propagate': True,
+        },  # Might as well log any errors anywhere else in Django
+        'django': {
+            'handlers': ['logfile'], 'level': 'ERROR',
+            'propagate': False,
         },
+        # Your own app - this assumes all your logger names start with "myapp."
+        'myapp': {
+            'handlers': ['logfile'],
+            'level': 'WARNING',  # Or maybe INFO or DEBUG
+            'propagate': False},
     },
 }
