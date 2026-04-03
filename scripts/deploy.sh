d09cb6db07a3991a6d8b6e686ed2703b0271f6fb
Angel Bejarano <programador.angel@gmail.com>
2026-04-03 08:28:42 -0300

    ci: use configurable sudo prefix for systemd commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,20 @@ BRANCH="${DEPLOY_BRANCH:-master}"
 APP_SERVICE="${APP_SERVICE:-coven-web}"
 CELERY_SERVICE="${CELERY_SERVICE:-coven-celery}"
 CELERY_BEAT_SERVICE="${CELERY_BEAT_SERVICE:-coven-celery-beat}"
-SYSTEMCTL_BIN="${SYSTEMCTL_BIN:-systemctl}"
+SYSTEMCTL_PREFIX="${SYSTEMCTL_PREFIX:-sudo}"
 
 cd "$PROJECT_DIR"
 
 if [[ ! -d "$VENV_DIR" ]]; then
   echo "Virtualenv not found at $VENV_DIR"
   exit 1
+fi
+
+if [[ -n "$SYSTEMCTL_PREFIX" ]]; then
+  read -r -a SYSTEMCTL_CMD <<< "$SYSTEMCTL_PREFIX"
+  SYSTEMCTL_CMD+=(systemctl)
+else
+  SYSTEMCTL_CMD=(systemctl)
 fi
 
 source "$VENV_DIR/bin/activate"
@@ -27,13 +34,13 @@ pip install gunicorn
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-"$SYSTEMCTL_BIN" daemon-reload
-"$SYSTEMCTL_BIN" restart "$APP_SERVICE"
-"$SYSTEMCTL_BIN" restart "$CELERY_SERVICE"
-"$SYSTEMCTL_BIN" restart "$CELERY_BEAT_SERVICE"
+"${SYSTEMCTL_CMD[@]}" daemon-reload
+"${SYSTEMCTL_CMD[@]}" restart "$APP_SERVICE"
+"${SYSTEMCTL_CMD[@]}" restart "$CELERY_SERVICE"
+"${SYSTEMCTL_CMD[@]}" restart "$CELERY_BEAT_SERVICE"
 
-"$SYSTEMCTL_BIN" is-active "$APP_SERVICE"
-"$SYSTEMCTL_BIN" is-active "$CELERY_SERVICE"
-"$SYSTEMCTL_BIN" is-active "$CELERY_BEAT_SERVICE"
+"${SYSTEMCTL_CMD[@]}" is-active "$APP_SERVICE"
+"${SYSTEMCTL_CMD[@]}" is-active "$CELERY_SERVICE"
+"${SYSTEMCTL_CMD[@]}" is-active "$CELERY_BEAT_SERVICE"
 
 echo "Deploy finished successfully on branch $BRANCH"
